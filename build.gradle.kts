653fd6c54852f6f3b422ae83cce245a6ed8ea8b8
alf3ratz <dr.mrqwerty1@yandex.ru>
2021-11-28 23:26:28 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "edu.hse"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
