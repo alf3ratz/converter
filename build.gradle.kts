77eeaf98ed58d7d43dec47dd72c63e9a99c52dca
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-12-04 13:42:00 +0300

    change gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,28 @@
 plugins {
     kotlin("jvm") version "1.5.31"
-}
+    application
 
+}
+application {
+    mainClass.set("MainKt")
+}
 group = "edu.hse"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+//sourceSets {
+//    main {
+//        java.srcDir("edu.hse.main.mainKt")
+//    }
+//}
+//tasks.withType<Jar> {
+//    manifest {
+//        attributes["Main-Class"] = "edu.hse.main.main"
+//    }
+//}
+
 
 dependencies {
     implementation(kotlin("stdlib"))
